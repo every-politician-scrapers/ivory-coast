f1875658e72490a9006d06e756d7ff77be44b8fb
Tony Bowden <tony@tmtm.com>
2022-11-07 16:08:48 +0200

    upgrade mirroring script

--- sources/cabinet/official/mirror.sh
+++ sources/cabinet/official/mirror.sh
@@ -5,9 +5,9 @@ cd $(dirname $0)
 if [[ $(jq -r .reference.P854 meta.json) == http* ]]
 then
   TMPFILE=$(mktemp)
-  CURLOPTS='-L -c /tmp/cookies -A eps/1.2'
+  CURLOPTS='-L -b /tmp/cookies -c /tmp/cookies --compressed --insecure'
 
-  curl $CURLOPTS -o $TMPFILE $(jq -r .reference.P854 meta.json)
+  curl $CURLOPTS -A 'Chrome/51.0.2704.103 Safari/537.36' -o $TMPFILE $(jq -r .reference.P854 meta.json)
 
   if grep -q "403 Forbidden" $TMPFILE; then
     echo "403 Forbidden"
